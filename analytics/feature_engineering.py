55bdb46f9e37578c313e3e3a814eb0492db78c61
kavitha098 <kavithasrinivas987@gmail.com>
2026-06-06 10:53:45 +0530

    Update feature_engineering.py

--- analytics/feature_engineering.py
+++ analytics/feature_engineering.py
@@ -1,1 +1,19 @@
+def add_rolling_features(df):
 
+    df = df.copy()
+
+    df["Temp_Rolling_Mean"] = (
+        df["Temperature (°C)"]
+        .rolling(window=10)
+        .mean()
+    )
+
+    df["Vib_Rolling_Mean"] = (
+        df["Vibration (m/s²)"]
+        .rolling(window=10)
+        .mean()
+    )
+
+    df.fillna(method="bfill", inplace=True)
+
+    return df
