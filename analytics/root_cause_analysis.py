bdfe083b8c37fc61376b334d77466f6f59350aad
kavitha098 <kavithasrinivas987@gmail.com>
2026-06-06 10:52:54 +0530

    Update root_cause_analysis.py

--- analytics/root_cause_analysis.py
+++ analytics/root_cause_analysis.py
@@ -1,1 +1,35 @@
 
+import pandas as pd
+
+
+def equipment_failure_analysis(df):
+
+    failure_df = df[df["Fault Detected"] == 1]
+
+    result = (
+        failure_df.groupby("Equipment_ID")
+        .size()
+        .reset_index(name="Failure_Count")
+        .sort_values("Failure_Count", ascending=False)
+    )
+
+    return result
+
+
+def failure_type_analysis(df):
+
+    return (
+        df.groupby("Failure Type")
+        .size()
+        .reset_index(name="Count")
+        .sort_values("Count", ascending=False)
+    )
+
+
+def critical_equipment_analysis(df):
+
+    return (
+        df.groupby("Equipment Criticality")
+        .size()
+        .reset_index(name="Count")
+    )
