259236a231809af661f903a0cf33e52fe4152fb9
kavitha098 <kavithasrinivas987@gmail.com>
2026-06-06 10:26:12 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,27 @@
+"""
+Models package for predictive maintenance
+"""
 
+from .train_models import (
+    train_linear_regression,
+    train_ridge_regression,
+    train_knn_regression,
+    train_decision_tree,
+    train_random_forest,
+    train_xgboost,
+    train_gradient_boosting,
+    train_svr,
+    evaluate_models
+)
+
+__all__ = [
+    'train_linear_regression',
+    'train_ridge_regression',
+    'train_knn_regression',
+    'train_decision_tree',
+    'train_random_forest',
+    'train_xgboost',
+    'train_gradient_boosting',
+    'train_svr',
+    'evaluate_models'
+]
