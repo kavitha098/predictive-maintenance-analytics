d13a001ef23615a7aff1e4c85c267a9675fc7535
kavitha098 <kavithasrinivas987@gmail.com>
2026-06-06 10:54:15 +0530

    Update visualization.py

--- analytics/visualization.py
+++ analytics/visualization.py
@@ -1,1 +1,25 @@
+import plotly.express as px
 
+
+def plot_temperature(df):
+
+    fig = px.line(
+        df,
+        x=df.index,
+        y="Temperature (°C)",
+        title="Temperature Trend"
+    )
+
+    return fig
+
+
+def plot_vibration(df):
+
+    fig = px.line(
+        df,
+        x=df.index,
+        y="Vibration (m/s²)",
+        title="Vibration Trend"
+    )
+
+    return fig
