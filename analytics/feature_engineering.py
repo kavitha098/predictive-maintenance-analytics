69d5329a66e26e8510afb92dcb8ea61aadfccdc3
kavitha098 <kavithasrinivas987@gmail.com>
2026-06-06 11:04:58 +0530

    Update feature_engineering.py

--- analytics/feature_engineering.py
+++ analytics/feature_engineering.py
@@ -1,19 +1,42 @@
+import pandas as pd
+import numpy as np
+
+
 def add_rolling_features(df):
 
     df = df.copy()
 
-    df["Temp_Rolling_Mean"] = (
-        df["Temperature (°C)"]
-        .rolling(window=10)
-        .mean()
-    )
+    if "Temperature (°C)" in df.columns:
 
-    df["Vib_Rolling_Mean"] = (
-        df["Vibration (m/s²)"]
-        .rolling(window=10)
-        .mean()
-    )
+        df["Temp_Rolling_Mean"] = (
+            df["Temperature (°C)"]
+            .rolling(window=10, min_periods=1)
+            .mean()
+        )
 
-    df.fillna(method="bfill", inplace=True)
+        df["Temp_Rolling_STD"] = (
+            df["Temperature (°C)"]
+            .rolling(window=10, min_periods=1)
+            .std()
+        )
+
+    if "Vibration (m/s²)" in df.columns:
+
+        df["Vib_Rolling_Mean"] = (
+            df["Vibration (m/s²)"]
+            .rolling(window=10, min_periods=1)
+            .mean()
+        )
+
+        df["Vib_Rolling_STD"] = (
+            df["Vibration (m/s²)"]
+            .rolling(window=10, min_periods=1)
+            .std()
+        )
+
+    numeric_cols = df.select_dtypes(include=np.number).columns
+
+    df[numeric_cols] = df[numeric_cols].bfill()
+    df[numeric_cols] = df[numeric_cols].ffill()
 
     return df
